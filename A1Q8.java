cb5fc4b48e964c54b4e8df8ce17a0ecf9ebf4109
Tr3x007 <49775288+Tr3x007@users.noreply.github.com>
2019-09-03 12:06:42 +0530

    Add files via upload

---
+++ A1Q8.java
@@ -0,0 +1,19 @@
+
+public class A1Q8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		a=30;
+		b=40;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("the value of a is = " + a);
+		System.out.println("the value of a is = " + b);
+
+
+	}
+
+}
